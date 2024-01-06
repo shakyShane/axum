afc44fbbf855990eb3a6445f14a50d514f8af6bb
Shane Osbourne <sosbourne@duckduckgo.com>
2024-01-06 20:36:19 +0000

    debugging

--- examples/reverse-proxy/src/main.rs
+++ examples/reverse-proxy/src/main.rs
@@ -96,7 +96,6 @@ async fn handler(
         .map(|v| v.as_str())
         .unwrap_or(path);
 
-    // let uri = format!("http://example.com{}", path_query);
     let Ok(uri) = Uri::builder()
         .scheme("http")
         .authority(config.host.clone())
@@ -124,20 +123,25 @@ async fn print_request_response(
     req: Request,
     next: Next,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let uri = req.uri().to_string();
     let (parts, body) = req.into_parts();
-    let bytes = buffer_and_print("request", body).await?;
+    let bytes = buffer_and_print("request", &uri, body).await?;
     let req = Request::from_parts(parts, Body::from(bytes));
 
     let res = next.run(req).await;
 
     let (parts, body) = res.into_parts();
-    let bytes = buffer_and_print("response", body).await?;
+    let bytes = buffer_and_print("response", &uri, body).await?;
     let res = Response::from_parts(parts, Body::from(bytes));
 
     Ok(res)
 }
 
-async fn buffer_and_print<B>(direction: &str, body: B) -> Result<Bytes, (StatusCode, String)>
+async fn buffer_and_print<B>(
+    direction: &str,
+    uri: &str,
+    body: B,
+) -> Result<Bytes, (StatusCode, String)>
 where
     B: axum::body::HttpBody<Data = Bytes>,
     B::Error: std::fmt::Display,
@@ -153,7 +157,7 @@ where
     };
 
     if let Ok(body) = std::str::from_utf8(&bytes) {
-        tracing::debug!("{direction} body = {body:?}");
+        tracing::debug!("{direction} {uri} body = {body:?}");
     }
 
     Ok(bytes)
